a4f69b4ff0272c210044124c74375740908b8f31
Nguyen Nguyen Phuong <60295691+npsables@users.noreply.github.com>
2021-10-09 10:44:04 +0700

    del dist

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-export * from './constants';
-export { default as account } from './account';
-export * from '../src/wallet';
-export { default as COMPONENTS } from './constants';
--- dist/wallet/index.js
+++
@@ -1,2 +0,0 @@
-export * from './solana';
-export * from './bitcoin_testnet';
