e3148f759a7fac64502f8e3085b40604d3796acf
npsables <phuongnguyen.ng3010@gmail.com>
2021-11-26 16:51:18 +0700

    add some path for thesis

--- test/solana.test.js
+++ test/solana.test.js
@@ -1,9 +1,9 @@
-const { account } = require('../dist')
+const { account, pathRegex } = require('../dist')
 const { payer } = require('./config')
 const chainID = 501
 
 describe('Account SOLANA', function () {
-  it('Should be a valid address', async function () {
+  it('Should be a valid mnemonic code', async function () {
     const ok = account.createMnemonic()
     if (!ok) throw new Error('Failed mnemonic creation')
   })
@@ -17,14 +17,20 @@ describe('Account SOLANA', function () {
   it('Should be valid master key pair', async function () {
     const a =  account.createMnemonic()    
     const seed = account.createSeed(a)
-    const ok = account.createMasterAccount(501, seed)
+    const ok = account.createMasterAccount(chainID, seed)
     if (!ok) throw new Error('Failed mnemonic creation')
+  })
+
+  it('Should be valid path', async function () {
+    const path = account.getPath(chainID, 2021)
+    // console.log(ok)
+    if (!path) throw new Error('Failed create path')
   })
 
   it('Should be valid child key pair', async function () {
     const a =  account.createMnemonic()    
     const seed = account.createSeed(a)
-    const path = account.getPath(501,10)
+    const path = account.getPath(chainID,10)
     // console.log(path)
     const ok = account.createChildAccount('501', seed, path)
     if (!ok) throw new Error('Failed mnemonic creation')
@@ -33,20 +39,20 @@ describe('Account SOLANA', function () {
   it('Should be valid public address', async function () {
     const a =  account.createMnemonic()    
     const seed = account.createSeed(a)
-    const path = account.getPath(501,10)
+    const path = account.getPath(chainID,10)
     // console.log(path)
-    const keys = account.createChildAccount(501, seed, path)
-    ok = account.getAddress(keys.pub, 501)
+    const keys = account.createChildAccount(chainID, seed, path)
+    ok = account.getAddress(keys.pub, chainID)
     if (!ok) throw new Error('Failed mnemonic creation')
   })
 
   it('Should be a child generated from given mnemonic', async function () {
     const mnem = payer.mnemonic
     const seed = account.createSeed(mnem)
-    const path = account.getPath(501,10)
+    const path = account.getPath(chainID,10)
     // console.log(path)
-    const keys = account.createChildAccount(501, seed, path)
-    const address = account.getAddress(keys.pub, 501) 
+    const keys = account.createChildAccount(chainID, seed, path)
+    const address = account.getAddress(keys.pub, chainID) 
     if (address != payer['m/44\'/501\'/0\'/0\'/10\'']) throw new Error('Failed mnemonic creation')
   })
 
